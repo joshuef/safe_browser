3fb3392347a38c593e1a6265787788d77cbdbc4b
Josh Wilson <joshuef@gmail.com>
2019-02-07 12:42:12 +0100

    for linux

--- post-pack.js
+++ post-pack.js
@@ -1,32 +1,29 @@
 #!/usr/bin/env node
-const exec = require( 'child_process' ).exec;
-const path = require( 'path' );
-const fs = require( 'fs-extra' );
-const archiver = require( 'archiver' );
+const path = require('path');
+const fs = require('fs-extra');
 
-const RELEASE_FOLDER_NAME = require( './releaseName' );
+const RELEASE_FOLDER_NAME = require('./releaseName');
 
-const pkg = require( './package.json' );
+const pkg = require('./package.json');
 
 const env = process.env.NODE_ENV || 'production';
-const isBuildingDev = /^dev/.test( env );
+const isBuildingDev = /^dev/.test(env);
 
-const targetDir = path.resolve( __dirname, 'release' );
+const targetDir = path.resolve(__dirname, 'release');
 
-const platform = process.platform;
+const { platform } = process;
 const OSX = 'darwin';
 const LINUX = 'linux';
 const WINDOWS = 'win32';
-const pkgName = pkg.name;
 
-let PLATFORM_NAME;
+// let PLATFORM_NAME;
 let CONTAINING_FOLDER;
 
-if ( platform === OSX )
-{
-    CONTAINING_FOLDER = path.resolve( targetDir, 'mac' );
-    const PERUSE_FOLDER = path.resolve( CONTAINING_FOLDER, 'SAFE Browser.app' );
-    const PERUSE_CONTENTS_FOLDER = path.resolve( PERUSE_FOLDER, 'Contents' );
+const LOGS = 'log.toml';
+if (platform === OSX) {
+    CONTAINING_FOLDER = path.resolve(targetDir, 'mac');
+    const PERUSE_FOLDER = path.resolve(CONTAINING_FOLDER, 'SAFE Browser.app');
+    const PERUSE_CONTENTS_FOLDER = path.resolve(PERUSE_FOLDER, 'Contents');
     const PERUSE_RESOURCES_FOLDER = path.resolve(
         PERUSE_CONTENTS_FOLDER,
         'Resources'
@@ -36,67 +33,78 @@ if ( platform === OSX )
         'Contents/Frameworks/SAFE Browser Helper.app/Contents/MacOS'
     );
 
-    const LOGS = 'log.toml';
+    fs.moveSync(
+        path.resolve(PERUSE_RESOURCES_FOLDER, 'SAFE Browser.crust.config'),
+        path.resolve(PERUSE_CONFIG_FOLDER, 'SAFE Browser Helper.crust.config'),
+        { overwrite: true }
+    );
 
     fs.moveSync(
-        path.resolve( PERUSE_CONTENTS_FOLDER, 'SAFE Browser.crust.config' ),
-        path.resolve( PERUSE_CONFIG_FOLDER, 'SAFE Browser Helper.crust.config' ),
+        path.resolve(PERUSE_RESOURCES_FOLDER, LOGS),
+        path.resolve(PERUSE_CONFIG_FOLDER, LOGS),
         { overwrite: true }
     );
 
-    fs.copySync(
-        path.resolve( PERUSE_CONTENTS_FOLDER, LOGS ),
-        path.resolve( PERUSE_CONFIG_FOLDER, LOGS ),
-        { overwrite: true }
-    );
+    // PLATFORM_NAME = 'osx';
 
-    PLATFORM_NAME = 'osx';
-
-    if ( isBuildingDev )
-    {
+    if (isBuildingDev) {
         fs.writeFileSync(
-            path.resolve( PERUSE_RESOURCES_FOLDER, 'startAsMock' ),
+            path.resolve(PERUSE_RESOURCES_FOLDER, 'startAsMock'),
             'unimportantContents'
         );
     }
 }
 
-if ( platform === LINUX )
-{
-    CONTAINING_FOLDER = path.resolve( targetDir, 'linux-unpacked' );
+if (platform === LINUX) {
+    CONTAINING_FOLDER = path.resolve(targetDir, 'linux-unpacked');
 
-    PLATFORM_NAME = LINUX;
+    // PLATFORM_NAME = LINUX;
+
+    const PERUSE_RESOURCES_FOLDER = path.resolve(
+        CONTAINING_FOLDER,
+        'Resources'
+    );
+
+    fs.moveSync(
+        path.resolve(PERUSE_RESOURCES_FOLDER, 'SAFE Browser.crust.config'),
+        path.resolve(CONTAINING_FOLDER, 'SAFE Browser Helper.crust.config'),
+        { overwrite: true }
+    );
+
+    fs.moveSync(
+        path.resolve(PERUSE_RESOURCES_FOLDER, LOGS),
+        path.resolve(CONTAINING_FOLDER, LOGS),
+        { overwrite: true }
+    );
 }
 
-if ( platform === WINDOWS )
-{
-    CONTAINING_FOLDER = path.resolve( targetDir, 'win-unpacked' );
+if (platform === WINDOWS) {
+    CONTAINING_FOLDER = path.resolve(targetDir, 'win-unpacked');
     const PERUSE_RESOURCES_FOLDER = path.resolve(
         CONTAINING_FOLDER,
         'resources'
     );
     fs.copySync(
-        path.resolve( PERUSE_RESOURCES_FOLDER, 'SAFE Browser.crust.config' ),
-        path.resolve( CONTAINING_FOLDER, 'SAFE Browser.crust.config' ),
+        path.resolve(PERUSE_RESOURCES_FOLDER, 'SAFE Browser.crust.config'),
+        path.resolve(CONTAINING_FOLDER, 'SAFE Browser.crust.config'),
         { overwrite: true }
     );
-    fs.unlinkSync( path.resolve( CONTAINING_FOLDER, 'safe-browser.crust.config' ) );
+    fs.unlinkSync(path.resolve(CONTAINING_FOLDER, 'safe-browser.crust.config'));
     fs.unlinkSync(
-        path.resolve( PERUSE_RESOURCES_FOLDER, 'SAFE Browser.crust.config' )
+        path.resolve(PERUSE_RESOURCES_FOLDER, 'SAFE Browser.crust.config')
     );
-    PLATFORM_NAME = 'win';
+    // PLATFORM_NAME = 'win';
 }
 
-if ( isBuildingDev && ( platform === WINDOWS || platform === LINUX ) )
-{
+if (isBuildingDev && (platform === WINDOWS || platform === LINUX)) {
     fs.writeFileSync(
-        path.resolve( CONTAINING_FOLDER, 'resources', 'startAsMock' ),
+        path.resolve(CONTAINING_FOLDER, 'resources', 'startAsMock'),
         'unimportantContents'
     );
 }
 
 // add version file
-fs.outputFileSync( path.resolve( CONTAINING_FOLDER, 'version' ), pkg.version );
+fs.outputFileSync(path.resolve(CONTAINING_FOLDER, 'version'), pkg.version);
 
 // remove licenses
 const removalArray = [
@@ -105,18 +113,17 @@ const removalArray = [
     'LICENSE'
 ];
 
-removalArray.forEach( file =>
-{
-    fs.removeSync( `${ CONTAINING_FOLDER }/${ file }` );
-} );
+removalArray.forEach(file => {
+    fs.removeSync(`${CONTAINING_FOLDER}/${file}`);
+});
 
 console.log(
     'Renaming package to:',
-    path.resolve( targetDir, `${ RELEASE_FOLDER_NAME }` )
+    path.resolve(targetDir, `${RELEASE_FOLDER_NAME}`)
 );
 // rename release folder
 fs.moveSync(
     CONTAINING_FOLDER,
-    path.resolve( targetDir, `${ RELEASE_FOLDER_NAME }` ),
+    path.resolve(targetDir, `${RELEASE_FOLDER_NAME}`),
     { overwrite: true }
 );
